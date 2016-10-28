40d264e673c2167d28dcc2a6a557421d1534e2f7
Tsinat Zeree <cheru87@gmail.com>
2016-10-28 01:48:43 -0700

    signin handled by backendserver

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,7 +1,10 @@
 
+import axios from 'axios';
+
+const ROOT_URL =   'http://localhost:3000';
 
 export function signinUser({ email, password }) {
     return function(dispatch) {
-        
+        axios.post(`${ROOT_URL}/signin`, { email, password })
     }
 }
--- client/components/auth/signin.js
+++ client/components/auth/signin.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
 
 class Signin extends Component {
     handleFormSubmit({ email, password}) {
         console.log(email, password);
+        this.props.signinUser({ email, password });
+
     }
     render() {
         const { handleSubmit, fields: { email, password }} = this.props;
@@ -26,4 +29,4 @@ class Signin extends Component {
 export default reduxForm({
     form: 'signin',
     fields: ['email', 'password']
-})(Signin);
+}, null, actions)(Signin);
