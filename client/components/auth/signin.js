1d9bb091fa925935d8e043120bb3558f246208ea
Tsinat Zeree <cheru87@gmail.com>
2016-11-04 00:09:05 -0700

    modification in error reducer

--- client/components/auth/signin.js
+++ client/components/auth/signin.js
@@ -31,7 +31,7 @@ class Signin extends Component {
                 </fieldset>
                 <fieldset className="form-group">
                     <label>Password:</label>
-                    <input {...password} className="form-control" />
+                    <input {...password} type="password"  className="form-control" />
                 </fieldset>
                 { this.renderAlert() }
                 <button action="submit" className="btn btn-primary">Sign in</button>
