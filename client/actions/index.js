4845cdde1066bbc590bd140c78e433a1d9b56663
Tsinat Zeree <cheru87@gmail.com>
2016-11-03 23:49:04 -0700

    saving jwt token to localstorage

--- client/actions/index.js
+++ client/actions/index.js
@@ -11,10 +11,11 @@ export function signinUser({ email, password }) {
         axios.post(`${ROOT_URL}/signin`, { email, password })
             .then(response => {
                 dispatch({type: AUTH_USER})
+                localStorage.setItem('token', response.data.token)
                 browserHistory.push('/feature');
             })
             .catch(error => {
-                
+
             });
     }
 }
