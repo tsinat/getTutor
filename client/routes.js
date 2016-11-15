19e2cb5ef20c8e7c6f46ea239575c647055fd279
Tsinat Zeree <cheru87@gmail.com>
2016-11-14 23:54:34 -0800

    require auth for  feature route

--- client/routes.js
+++ client/routes.js
@@ -6,13 +6,14 @@ import HomePage from './components/HomePage';
 import Feature from './components/feature';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
+import RequireAuth from './components/auth/require_auth';
 import Signout from './components/auth/signout';
 
 export default (
     <Route path='/' component={App}>
         <IndexRoute  component={HomePage}/>
         <Route path='categories' component={Signin} />
-        <Route path='feature' component={Feature} />
+        <Route path='feature' component={RequireAuth(Feature)} />
         <Route path='signin' component={Signin} />
         <Route path='signup' component={Signup} />
         <Route path='signout' component={Signout} />
