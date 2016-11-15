34bf982429d876a64eaa007525583a0c3afaddc6
Tsinat Zeree <cheru87@gmail.com>
2016-11-14 23:40:52 -0800

    higher order component to protect route

---
+++ client/components/feature.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+    render() {
+        return (
+            <div>This is a  feature</div>
+        )
+    }
+}
+
+export default Feature;
--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/App';
 import HomePage from './components/HomePage';
+import Feature from './components/feature';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import Signout from './components/auth/signout';
@@ -11,7 +12,7 @@ export default (
     <Route path='/' component={App}>
         <IndexRoute  component={HomePage}/>
         <Route path='categories' component={Signin} />
-        <Route path='profile' component={Signin} />
+        <Route path='feature' component={Feature} />
         <Route path='signin' component={Signin} />
         <Route path='signup' component={Signup} />
         <Route path='signout' component={Signout} />
