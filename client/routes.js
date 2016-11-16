543b1e1a3f802d85aa2bddadecb3b55151c61979
Tsinat Zeree <cheru87@gmail.com>
2016-11-15 23:32:34 -0800

    added profile component

--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/App';
 import HomePage from './components/HomePage';
-import Feature from './components/feature';
+import Profile from './components/feature';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import RequireAuth from './components/auth/require_auth';
@@ -13,7 +13,7 @@ export default (
     <Route path='/' component={App}>
         <IndexRoute  component={HomePage}/>
         <Route path='categories' component={Signin} />
-        <Route path='feature' component={RequireAuth(Feature)} />
+        <Route path='profile' component={RequireAuth(Profile)} />
         <Route path='signin' component={Signin} />
         <Route path='signup' component={Signup} />
         <Route path='signout' component={Signout} />
