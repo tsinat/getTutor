037880815f63cbeff8dfced65e94446127210de6
Tsinat Zeree <cheru87@gmail.com>
2016-11-16 23:51:53 -0800

    clean up

--- client/components/auth/signout.js
+++ client/components/auth/signout.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
-
 class Signout extends Component {
     componentWillMount() {
         this.props.signoutUser();
-    }    
+    }
     render() {
         return (
             <div>
