edc24a42779c5d61342ebc4767189b18fdccaa1f
Tsinat Zeree <cheru87@gmail.com>
2016-11-18 12:15:07 -0800

    complete refactor of navigation

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -3,56 +3,76 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            collapsed: true
+        };
+        this.toggleCollapse = this.toggleCollapse.bind(this);
+    }
+    toggleCollapse() {
+        const collapsed = !this.state.collapsed;
+        this.setState({collapsed});
+    }
     renderLinks() {
         if(this.props.authenticated){
             return [
                 <li className="nav-item" key={1}>
-                    <Link className="nav-link" to='/categories'>Categories</Link>
+                    <Link to='/categories' className="nav-link" onClick={this.toggleCollapse}>Categories</Link>
                 </li>,
                 <li className="nav-item" key={2}>
-                    <Link className="nav-link" to='/profile'>Profile</Link>
+                    <Link to='/profile' className="nav-link" onClick={this.toggleCollapse} >Profile</Link>
                 </li>,
                 <li className="nav-item" key={3}>
-                    <Link className="nav-link" to='/signout'>Sign Out</Link>
+                    <Link to='/signout' className="nav-link" onClick={this.toggleCollapse} >Sign Out</Link>
                 </li>,
                 <li className="nav-item" key={4}>
-                    <Link className="nav-link" to='/help'>Help?</Link>
+                    <Link to='/help' className="nav-link" onClick={this.toggleCollapse} >Help?</Link>
                 </li>
             ]
         } else {
             return [
                 <li className="nav-item" key={1}>
-                    <Link className="nav-link" to='/categories'>Categories</Link>
+                    <Link to='/categories' className="nav-link" onClick={this.toggleCollapse} >Categories</Link>
                 </li>,
                 <li className="nav-item" key={2}>
-                    <Link className="nav-link" to='/signup'>Sign Up</Link>
+                    <Link to='/signup' className="nav-link" onClick={this.toggleCollapse} >Sign Up</Link>
                 </li>,
                 <li className="nav-item" key={3}>
-                    <Link className="nav-link" to='/signin'>Sign In</Link>
+                    <Link to='/signin' className="nav-link" onClick={this.toggleCollapse} >Sign In</Link>
                 </li>,
                 <li className="nav-item" key={4}>
-                    <Link className="nav-link" to='/help'>Help?</Link>
+                    <Link to='/help' className="nav-link" onClick={this.toggleCollapse} >Help?</Link>
                 </li>
             ]
         }
 
     }
     render() {
-
+        const { collapsed } = this.state;
+        const navClass = collapsed ? "collapse" : "";
         return (
-            <nav className='navbar navbar-default'>
-                <div className='container-fluid'>
-                    <div className='navbar-header'>
+            <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+              <div className="container-fluid">
+                <div className="navbar-header">
+                  <button type="button" className="navbar-toggle" onClick={this.toggleCollapse}>
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                  <div className='navbar-header'>
                         <Link to='/' className='navbar-brand'>getTutor</Link>
-                    </div>
-                    <div className='collapse navbar-collapse'>
-                        <ul className='nav navbar-nav navbar-right'>
-                            { this.renderLinks() }
-                        </ul>
-                    </div>
+                  </div>
                 </div>
+                <div className={ "navbar-collapse" + navClass } id="bs-example-navbar-collapse-1">
+                  <ul className="nav navbar-nav navbar-right">
+                    { this.renderLinks() }
+                  </ul>
+                </div>
+              </div>
             </nav>
-        )
+        );
 
     }
 }
