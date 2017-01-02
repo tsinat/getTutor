5be7e925fc8d2c1e7e0ebe9c8fb4ec9d45626bc4
Tsinat Zeree <cheru87@gmail.com>
2017-01-02 14:27:22 -0800

    addition of images in public folder

--- client/data/categoriesData.js
+++ client/data/categoriesData.js
@@ -3,37 +3,37 @@ const categoryList = [
     title: "Web Development",
     id: "web_development",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
-    image: "https://content-static.upwork.com/blog/uploads/sites/4/2014/03/web-development-cropped.jpg"
+    image: "/public/images/web-development-cropped.jpg"
     },
     {
     title: "Networking",
     id: "newtworking",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
-    image: "https://www.ccny.cuny.edu/sites/default/files/styles/top_slider/public/cintbanner.jpg"
+    image: "/public/images/cintbanner.jpg"
     },
     {
     title: "Graphic Design",
     id: "graphic_design",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
-    image: "http://cdn.skilledup.com/wp-content/uploads/2014/11/graphic-design-tools-Feature_1290x688_MS.jpg"
+    image: "/public/images/graphic-design-tools-Feature_1290x688_MS.jpg"
     },
     {
     title: "Highschool Tutor",
     id: "highschool_tutor",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
-    image: "http://www.greatschools.org/gk/wp-content/uploads/2010/01/Looking-for-a-tutor-750x325.jpg"
+    image: "/public/images/Looking-for-a-tutor-750x325.jpg"
     },
     {
     title: "Language Mentor",
     id: "language_mentor",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
-    image: "http://mliesl.edu/images/IMG_1464.jpg"
+    image: "/public/images/499343530.jpg"
     },
     {
     title: "Vocational Training",
     id: "vocational_training",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.",
-    image: "http://www.ncianet.org/wp-content/uploads/2011/06/DSC_0663.jpg"
+    image: "/public/images/DSC_0663.jpg"
     }
 ];
 
